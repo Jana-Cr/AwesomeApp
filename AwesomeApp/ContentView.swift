811d5deb97ceb52cffa2fc26e9e5e9a5f83a1ea0
Janai Cruz <jancr2025@gmail.com>
2025-11-21 10:35:30 -0800

    test

--- AwesomeApp/ContentView.swift
+++ AwesomeApp/ContentView.swift
@@ -8,17 +8,61 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var message=""
+    @State private var imageName=""
+    @State private var lastMessageNumber = -1
+    @State private var lastImageNumber = -1
+    
     var body: some View {
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text(message)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .foregroundStyle(.red)
+                .multilineTextAlignment(.center)
+                .minimumScaleFactor(0.5)
+                .frame(height: 100)
+                .animation(.easeInOut(duration: 0.15), value: message)
+            
+            
+            Image(imageName)
+                .resizable()
+                .scaledToFit()
+                .clipShape(RoundedRectangle(cornerRadius: 30))
+                .shadow(radius:30)
+                .animation(.default, value: imageName)
+            
+            Spacer()
+            
+            Button("Show Message"){
+                let messages = ["You Are Awesome!", "You are great!", "You Are Fantastic!", "Fabulous? Thats You!", "You Make Me Smile!", "When The Genius Bar Needs Help, They Call You!","YAY"]
+                
+                var messageNumber = Int.random(in: 0...messages.count-1)
+                while messageNumber == lastMessageNumber{
+                    messageNumber = Int.random(in: 0...messages.count-1)
+                }
+                message = messages[messageNumber]
+                lastMessageNumber = messageNumber
+                
+                var imageNumber = Int.random(in: 0...9)
+                while imageNumber == lastImageNumber{
+                    imageNumber = Int.random(in: 0...9)
+                }
+                imageName = "image\(imageNumber)"
+                lastImageNumber = imageNumber
+                
+                            }
+            .buttonStyle(.borderedProminent)
+            .font(.title2)
+            
         }
         .padding()
     }
+    
 }
-
-#Preview {
+#Preview() {
     ContentView()
 }
+
+//this is a test
