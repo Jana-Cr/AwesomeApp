25b4c625a7104b5b87eb8df7176ea5d76e2270bc
Janai Cruz <jancr2025@gmail.com>
2025-11-21 10:32:53 -0800

    Initial Commit

---
+++ AwesomeApp/AwesomeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AwesomeAppApp.swift
+//  AwesomeApp
+//
+//  Created by CRUZ, JANAI on 11/21/25.
+//
+
+import SwiftUI
+
+@main
+struct AwesomeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
