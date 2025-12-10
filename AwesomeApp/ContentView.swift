edf7ebe978ad6d2e482c714aa2239f9c92b5f145
Janai Cruz <jancr2025@gmail.com>
2025-12-10 10:46:49 -0800

    Added a Off switch for the sound, allowing the user to toggle the sound off and on at any time. As well as keeping the sounds from repeating and allowing only one sound to play at a time.

--- AwesomeApp/ContentView.swift
+++ AwesomeApp/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
     @State private var lastMessageNumber = -1
     @State private var lastImageNumber = -1
     @State private var lastSoundNumber = -1
+    @State private var soundIsOn = true
     @State private var audioPlayer: AVAudioPlayer!
     let numberOfImages = 10 //images labeled image 0-9
     let numberOfSounds = 6 //sounds 0-5
@@ -42,38 +43,57 @@ struct ContentView: View {
             
             Spacer()
             
-            Button("Show Message"){
-                let messages = ["You Are Awesome!", "You are great!", "You Are Fantastic!", "Fabulous? Thats You!", "You Make Me Smile!", "When The Genius Bar Needs Help, They Call You!","YAY"]
+            HStack {
+                Text("Sound On:")
+                Toggle("", isOn: $soundIsOn)
+                    .labelsHidden()
+                    .onChange(of: soundIsOn) {
+                        if audioPlayer != nil && audioPlayer.isPlaying {
+                            audioPlayer.stop()
+                        }
+                    }
                 
-                lastMessageNumber = nonRepeatingRandom(lastNumber: lastMessageNumber, upperBound: messages.count-1)
-                message = messages[lastMessageNumber]
+                Spacer()
                 
-      
-                lastImageNumber = nonRepeatingRandom(lastNumber: lastImageNumber, upperBound: numberOfImages-1)
-                imageName = "image\(lastImageNumber)"
-
-                
-                lastSoundNumber = nonRepeatingRandom(lastNumber: lastSoundNumber, upperBound: numberOfSounds-1)
-                playSound(soundName: "sound\(lastSoundNumber)")
-               
-                            }
-            .buttonStyle(.borderedProminent)
-            .font(.title2)
+                Button("Show Message"){
+                    let messages = ["You Are Awesome!", "You are great!", "You Are Fantastic!", "Fabulous? Thats You!", "You Make Me Smile!", "When The Genius Bar Needs Help, They Call You!","YAY"]
+                    
+                    lastMessageNumber = nonRepeatingRandom(lastNumber: lastMessageNumber, upperBound: messages.count-1)
+                    message = messages[lastMessageNumber]
+                    
+                    
+                    lastImageNumber = nonRepeatingRandom(lastNumber: lastImageNumber, upperBound: numberOfImages-1)
+                    imageName = "image\(lastImageNumber)"
+                    
+                    
+                    lastSoundNumber = nonRepeatingRandom(lastNumber: lastSoundNumber, upperBound: numberOfSounds-1)
+                    if soundIsOn {
+                        playSound(soundName: "sound\(lastSoundNumber)")
+                    }
+                    
+                    
+                }
+                .buttonStyle(.borderedProminent)
+                .font(.title2)
+            }
             
         }
         .padding()
     }
     
-    func nonRepeatingRandom (lastNumber: Int, upperBound:Int)-> Int{
-        var newNumber:Int
-        repeat{
-            newNumber = Int.random(in:0...upperBound)
-        }while newNumber == lastMessageNumber
+    func nonRepeatingRandom(lastNumber: Int, upperBound:Int) -> Int {
+        var newNumber: Int
+        repeat {
+            newNumber = Int.random(in: 0...upperBound)
+        } while newNumber == lastNumber
         return newNumber
         
     }
     
     func playSound(soundName: String) {
+        if audioPlayer != nil && audioPlayer.isPlaying {
+            audioPlayer.stop()
+        }
         guard let soundFile = NSDataAsset(name: soundName) else {
             print("🥀Could not read file named \(soundName)")
             return
@@ -82,7 +102,7 @@ struct ContentView: View {
             audioPlayer = try AVAudioPlayer(data: soundFile.data)
             audioPlayer.play()
         }catch {
-            print("🥀 ERROR: \(error.localizedDescription)")
+            print("🥀 ERROR: \(error.localizedDescription) creating audioPlayer")
         }
         
     }
